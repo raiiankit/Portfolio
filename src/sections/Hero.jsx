d73a9ddfb7d3e16a3b3eeac540421dfbc8d81275
Ankit <ankit.rai@bypeopletechnologies.com>
2025-05-12 14:57:24 +0530

    update

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -26,7 +26,7 @@ const Hero = () => {
     <section className="min-h-screen w-full flex flex-col relative" id="home">
       <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
         <p className="sm:text-3xl mt-[-11px] text-2xl font-medium text-white text-center font-generalsans">
-          Hii, I'm Ankit Rai
+          Hi! , I'm Ankit Rai
         </p>
 
         <p className="hero_tag text-gray_gradient">
