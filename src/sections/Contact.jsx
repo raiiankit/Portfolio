c65231bfa258ef20886c857e4f32a8085463485b
Ankit <ankit.rai@bypeopletechnologies.com>
2025-06-24 18:15:10 +0530

    url update !!

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -21,7 +21,7 @@ const Contact = () => {
     e.preventDefault();
   
     try {
-      const response = await fetch('http://localhost:5000/api/contact', {
+      const response = await fetch('https://portfolio-backend-364d.onrender.com', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
